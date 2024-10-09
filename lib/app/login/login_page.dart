cab8f6847921320a64e71ab5f258cf8a4d7dd3f2
Arkadiusz Antonkiewicz <arkadiusz.antonkiewicz@op.pl>
2024-10-09 17:18:13 +0200

    Add error handling to LoginPage

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -1,13 +1,21 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   LoginPage({
     super.key,
   });
 
   final emailController = TextEditingController();
+
   final passwordController = TextEditingController();
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var errorMessage = '';
 
   @override
   Widget build(BuildContext context) {
@@ -23,14 +31,18 @@ class LoginPage extends StatelessWidget {
                 height: 20,
               ),
               TextField(
-                controller: emailController,
+                controller: widget.emailController,
                 decoration: const InputDecoration(hintText: 'E-mail'),
               ),
               TextField(
-                controller: passwordController,
+                controller: widget.passwordController,
                 decoration: const InputDecoration(hintText: 'Hasło'),
                 obscureText: true,
               ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(errorMessage),
               const SizedBox(
                 height: 20,
               ),
@@ -38,10 +50,14 @@ class LoginPage extends StatelessWidget {
                 onPressed: () async {
                   try {
                     await FirebaseAuth.instance.signInWithEmailAndPassword(
-                      email: emailController.text,
-                      password: passwordController.text,
+                      email: widget.emailController.text,
+                      password: widget.passwordController.text,
                     );
-                  } catch (error) {}
+                  } catch (error) {
+                    setState(() {
+                      errorMessage = error.toString();
+                    });
+                  } // treść błędu i obsługa jego stanu, który idzie wyżej do Text(errorMessage)
                 },
                 child: const Text('Zaloguj się'),
               ),
